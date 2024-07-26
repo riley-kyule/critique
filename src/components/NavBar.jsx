91c88985803f3bcf707ffe920996f5b1ad40ac26
riley-kyule <phillipkyule@gmail.com>
2024-07-26 08:41:22 +0300

    Revert "Django App"
    This reverts commit fdebf24fa91dbc65e4d0e27f8baf6fab454fa944.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <nav className="bg-violet-950 fixed w-full z-20 top-0 start-0">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-evenly mx-auto p-4">
     <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">Critique Blog</span>
+      <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">DevLogics</span>
     </Link>
     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
       <Link to="/add-blog">
